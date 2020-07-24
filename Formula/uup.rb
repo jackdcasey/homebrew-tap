d5ffe56414a40bcd34199184160f8f9553291f90
Jack Casey <jackdcasey@gmail.com>
2020-07-23 20:39:43 -0700

    Fix trailing comma

--- Formula/uup.rb
+++ Formula/uup.rb
@@ -1,7 +1,7 @@
 class Uup < Formula
     desc "A fast and lightweight ping alternative for macOS"
     homepage "https://github.com/jackdcasey/uup"
-    url "https://github.com/jackdcasey/uup/releases/download/v1.0/uup-darwin-amd64.tar.gz",
+    url "https://github.com/jackdcasey/uup/releases/download/v1.0/uup-darwin-amd64.tar.gz"
     sha256 "57a03d16aa4ea1d0a9e55a2a7f3b28e785a479b5847659358e951344b3ddd5a2"
     head "https://github.com/jackdcasey/uup.git"
 
