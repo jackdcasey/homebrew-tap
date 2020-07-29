df80fa52cf22f57e1476e4454ea9ac1711adb4c4
Jack Casey <jackdcasey@gmail.com>
2020-07-29 08:24:54 -0700

    Add version number to formula

--- Formula/uup.rb
+++ Formula/uup.rb
@@ -3,6 +3,7 @@ class Uup < Formula
     homepage "https://github.com/jackdcasey/uup"
     url "https://github.com/jackdcasey/uup/releases/download/v1.1/uup-darwin-amd64.tar.gz"
     sha256 "d6fe2bd82aeb1bf1351df200d3b62094a3f19e3d0503ee325cccf4e026b6e0bf"
+    version "1.1.0"
     head "https://github.com/jackdcasey/uup.git"
 
     def install
