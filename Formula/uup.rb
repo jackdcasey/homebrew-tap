a163d0e83ff92dfe20b6d3690b5361b00586458e
Jack Casey <jackdcasey@gmail.com>
2020-12-05 18:38:21 -0800

    Fix sha256

--- Formula/uup.rb
+++ Formula/uup.rb
@@ -2,7 +2,7 @@ class Uup < Formula
     desc "A fast and lightweight ping alternative for macOS"
     homepage "https://github.com/jackdcasey/uup"
     url "https://github.com/jackdcasey/uup/releases/download/v1.2/uup-darwin-amd64.tar.gz"
-    sha256 "c67a09d65d241d716c5005cd9c7f16f6ec1a8bd24aa5bd958b666087cf30791a"
+    sha256 "e3c0f7c4ae4b25810f11ee229dd6653c49f3b9eee0c27091361e7967429bcf01"
     version "1.2"
     head "https://github.com/jackdcasey/uup.git"
 
