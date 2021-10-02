dc83f8b61af035e5a81da2b9a857cbaf3cca49c4
Jack Casey <jackdcasey@gmail.com>
2021-10-01 22:07:02 -0700

    Iterate version to 1.5

--- Formula/uup.rb
+++ Formula/uup.rb
@@ -1,9 +1,9 @@
 class Uup < Formula
     desc "A fast and lightweight ping alternative for macOS"
     homepage "https://github.com/jackdcasey/uup"
-    url "https://github.com/jackdcasey/uup/releases/download/v1.4/uup-darwin-amd64.tar.gz"
-    sha256 "798b551cc61ea4ced44447b24cf843b812190509a72cd0b3f4d0bedcea01127d"
-    version "1.4"
+    url "https://github.com/jackdcasey/uup/releases/download/v1.5/uup-darwin-amd64.tar.gz"
+    sha256 "72af60850c37b7ff97ffb8b3a15cd27585485984d1cf2655f0adee68d62f00bc"
+    version "1.5"
     head "https://github.com/jackdcasey/uup.git"
 
     def install
